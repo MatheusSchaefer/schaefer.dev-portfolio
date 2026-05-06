6e5934963a1f0bcd502a09d5fdbcb51921ccd8cd
Matheus Schaefer <schaeferinvestimentos@gmail.com>
2026-05-05 21:16:49 -0300

    Primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const letras = "01";
+const fontSize = 16;
+const colunas = canvas.width / fontSize;
+
+const gotas = [];
+
+for (let i = 0; i < colunas; i++) {
+  gotas[i] = 1;
+}
+
+function desenharMatrix() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.08)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#00ff41";
+  ctx.font = fontSize + "px monospace";
+
+  for (let i = 0; i < gotas.length; i++) {
+    const texto = letras.charAt(Math.floor(Math.random() * letras.length));
+
+    ctx.fillText(texto, i * fontSize, gotas[i] * fontSize);
+
+    if (gotas[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      gotas[i] = 0;
+    }
+
+    gotas[i]++;
+  }
+}
+
+setInterval(desenharMatrix, 50);
